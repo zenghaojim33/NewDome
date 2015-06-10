b9c519bfb27f9242b953fbefb7a6b1d544efa9c9
zenghaojim33 <zenghaojim33@qq.com>
2015-06-10 11:10:47 +0800

    Initial Commit

---
+++ NewDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDome
+//
+//  Created by Anson on 15/6/10.
+//  Copyright (c) 2015年 Anson Tsang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
